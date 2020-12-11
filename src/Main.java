2c633d6a915f4a519f7eafe233bc378d0683487f
Caesar-PT <73921520+Caesar-PT@users.noreply.github.com>
2020-12-12 00:09:54 +0700

    f

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 2;
+        while (number < 100) {
+            boolean check = true;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check)
+                System.out.print(number + "\t");
+            number++;
+        }
+    }
+}
